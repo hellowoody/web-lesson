fe41016aa0902b958fe77d65c80e6edf4cc89977
woody <woodyhello@qq.com>
2020-09-01 11:03:31 +0800

    update

---
+++ vue-cli/shop-app/src/components/tabbar/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tab-bar tab-bar-extend">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TabBar',
+}
+</script>
+
+<style scoped>
+.tab-bar {
+  height:58px;
+  background-color: #fff;
+  position: fixed;
+  bottom:0px;
+  left:0px;
+  right:0px;
+  box-shadow: 0px 0px 8px #E3E3E3;
+}
+
+.tab-bar-extend {
+    display: flex;
+    align-items: center;
+}
+
+
+</style>
---
+++ vue-cli/shop-app/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tab-bar-item">
+        <div style="font-size:19px;margin-bottom:6px;" class="iconfont icon-home"></div>
+        <div style="font-size:14px">首页</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TabBarItem',
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex:1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
