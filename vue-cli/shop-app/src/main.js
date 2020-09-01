42f1e7154686995198fc8fd621370e0b88179f88
woody <woodyhello@qq.com>
2020-09-01 16:43:17 +0800

    update_20200901_6_woody

--- vue-cli/shop-app/src/main.js
+++ vue-cli/shop-app/src/main.js
@@ -5,12 +5,13 @@ import Home from '@/components/tabs/Home';
 import Product from '@/components/tabs/Product';
 import Order from '@/components/tabs/Order';
 import Account from '@/components/tabs/Account';
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter) 
 
 const routes = [
-  {path:'/',component:Home},
+  {path:'/',redirect:{path:"/home"}},
   {path:'/home',component:Home},
   {path:'/product',component:Product},
   {path:'/order',component:Order},
