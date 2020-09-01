439860e571a5e5b6c18e9bdfc1e5f50de713b9cb
woody <woodyhello@qq.com>
2020-09-01 11:05:36 +0800

    update

---
+++ vue-cli/shop-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <tab-bar>
+        <tab-bar-item />
+        <tab-bar-item/>
+        <tab-bar-item/>
+        <tab-bar-item/>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/tabbar/TabBar.vue'
+import TabBarItem from '@/components/tabbar/TabBarItem.vue'
+
+export default {
+  name:"app",
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/commom.css";
+@import "./assets/icon/iconfont.css";
+
+
+</style>
