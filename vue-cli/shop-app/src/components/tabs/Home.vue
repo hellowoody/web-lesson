b781bbf6e41010650b8543f376d1235fb1a1daf5
woody <woodyhello@qq.com>
2020-09-02 14:41:57 +0800

    update_20200902_2_woody_topbar起步

--- vue-cli/shop-app/src/components/tabs/Home.vue
+++ vue-cli/shop-app/src/components/tabs/Home.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>this is A Page</div>
+    <div>
+        <top-bar>
+            <div slot="middle">中间的搜索框</div>
+            <div slot="right" class="iconfont icon-gouwuchezhengpin" style="font-size: 24px"></div>
+        </top-bar>
+        <div>this is A Page</div>
+    </div>
 </template>
 
 <script>
+import TopBar from '@/components/topbar/TopBar'
+
 export default {
-    name:'A'
+    name:'A',
+    components:{
+        TopBar,
+    }
 }
 </script>
---
+++ vue-cli/shop-app/src/components/topbar/TopBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="top-bar">
+        <div class="left">
+            <slot name="left"></slot>
+        </div>
+        <div class="middle">
+             <slot name="middle"></slot>
+        </div>
+        <div class="right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TopBar',
+
+}
+</script>
+
+<style scoped>
+.top-bar {
+    background-color:#fff;
+    height:60px;
+    box-shadow: 0px 1px 2px rgba(40,40,40,0.2);
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 18px 0px 18px;
+}
+
+.left {
+    font-size: 24px;
+}
+
+.right {
+    font-size: 24px !important;
+}
+</style>
