a1f6373a4ea0f0adc5a908bf26140b0831d5fe2a
woody <woodyhello@qq.com>
2020-09-15 14:36:24 +0800

    update_20200915_5_woody

--- server/project-demo-ts/build/server.js
+++ server/project-demo-ts/build/server.js
@@ -7,10 +7,11 @@ var http_1 = __importDefault(require("http"));
 var url_1 = __importDefault(require("url"));
 http_1.default.createServer(function (req, resp) {
     if (req.url !== '/favicon.ico') {
-        var url_parse = url_1.default.parse(req.url !== undefined ? req.url : "");
+        var url_parse = url_1.default.parse(req.url !== undefined ? req.url : "", true);
         console.log(url_parse.query);
+        console.log(url_parse.query.a);
         resp.writeHead(200, { "Content-type": "text/html;charset=utf-8" });
         resp.write("你好！");
     }
 }).listen(3000);
-console.log("server is running at http://localhost:3000 ");
+console.log("server is running at http://localhost:3000?a= ");
--- server/project-demo-ts/src/server.ts
+++ server/project-demo-ts/src/server.ts
@@ -3,11 +3,12 @@ import url from 'url';
 
 http.createServer((req,resp)=>{
     if(req.url !== '/favicon.ico'){
-        const url_parse : url.UrlWithStringQuery = url.parse(req.url !== undefined ? req.url : "")
+        const url_parse : url.UrlWithParsedQuery = url.parse(req.url !== undefined ? req.url : "",true)
         console.log(url_parse.query)
+        console.log(url_parse.query.a)
         resp.writeHead(200,{"Content-type":"text/html;charset=utf-8"})
         resp.write("你好！")
     }
 }).listen(3000)
 
-console.log("server is running at http://localhost:3000 ")
+console.log("server is running at http://localhost:3000?a= ")
