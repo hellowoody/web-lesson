6b587a1ed8b5a9aa168ed3717e7b16025506af8e
woody <woodyhello@qq.com>
2020-09-29 11:03:28 +0800

    update

--- vue-cli/shop-app/src/components/pages/GoodDetail.vue
+++ vue-cli/shop-app/src/components/pages/GoodDetail.vue
@@ -10,6 +10,14 @@
                 <div><h3>2</h3></div>
                 <div><h3>3</h3></div>
             </a-carousel>
+            <div style="margin-top:30px;padding-left:5px">
+                <div class="goodtitle">goodtitle - gooddesc</div>
+                <div class="goodprice">¥ 1000.00</div>
+                <div style="color:rgb( 0 0 0 / 0.5);font-size:14px;font-weight:bold;margin-top:38px;">相近商品</div>
+                <div class="related-goods">
+                    <product-card class="related-goods-item" v-for="(item,index) in products" :key="index" :product="item"></product-card>
+                </div>
+            </div>
         </my-content>
         <footer-bar>
             <FooterBarButton slot="left" name="加入购物车" style="border:none" @funcHandle="addCart" />
@@ -23,14 +31,27 @@ import TopBar from '@/components/topbar/TopBar'
 import FooterBar from '@/components/footerbar/FooterBar'
 import FooterBarButton from '@/components/footerbar/FooterBarButton'
 import MyContent from '@/components/content/MyContent'
-
+import ProductCard from '@/components/product/ProductCard'
+let products = [
+   {},
+   {},
+   {},
+   {},
+   {},
+]
 export default {
     name:"GoodDetail",
+    data(){
+        return {
+            products
+        }
+    },
     components:{
         TopBar, 
         FooterBar,
         FooterBarButton,
-        MyContent
+        MyContent,
+        ProductCard
     },
     methods:{
         back(){
@@ -66,4 +87,29 @@ export default {
     font-size:18px;
     color:#8C8C8C
 }
+
+.goodtitle{
+    font-size:16px;
+    font-weight: bold;
+    color: rgb(0 0 0 / 0.5)
+}
+
+.goodprice{
+    font-size:18px;
+    font-weight: bold;
+    color:#FA6400;
+    margin-top:15px;
+}
+
+.related-goods{
+    display:flex;
+    overflow: auto;
+    margin-top:18px;
+}
+
+.related-goods-item {
+    flex-shrink: 0;
+    margin-right: 12px;
+}
+
 </style>
