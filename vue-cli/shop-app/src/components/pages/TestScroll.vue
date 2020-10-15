8e9d3164ae156b90f6ae0e6d0612c38cb9baf140
woody <woodyhello@qq.com>
2020-10-15 09:22:14 +0800

    update

--- vue-cli/shop-app/src/components/pages/TestScroll.vue
+++ vue-cli/shop-app/src/components/pages/TestScroll.vue
@@ -3,15 +3,16 @@
     <top-bar>
        <div slot="left" style="font-size:24px;" class="iconfont icon-back1" @click="back"></div>
       <div slot="middle">测试 左右滚动</div>
+      <div slot="right" @click="test">向上滑动</div>
     </top-bar>
     <div class="content">
       <div class="left-wrapper">
-        <div v-for="item in categorys" :key="item" style="height:60px;">{{item}}</div>
+        <div v-for="(item,index) in categorys" :key="item" :class="[activeCategoryIndex === index ? 'left-wrapper-item-active' : 'left-wrapper-item' ]"  @click="selectedHandle(index)">{{item}}</div>
       </div>
-      <div class="right-wrapper">
-        <div v-for="item in detailList" :key="item.name" >
+      <div class="right-wrapper" ref="right">
+        <div v-for="item in detalList" :key="item.name" class="right-wrapper-item">
           <div v-for="(t,index) in item.list" :key="item.name+index" style="height:30px">
-            <span v-if="index === 0 ">{{item.name + "-" + t.name}}</span>
+            <span v-if="index === 0">{{t.name + "-" + item.name}}</span>
             <span v-else>{{t.name}}</span>
           </div>
         </div>
@@ -27,6 +28,7 @@ export default {
   name:"TestScroll",
   data(){
     return {
+      activeCategoryIndex:0,
       categorys:[
         "类别1",
         "类别2",
@@ -35,7 +37,7 @@ export default {
         "类别5",
         "类别6",
       ],
-      detailList:[
+      detalList:[
         {
           name:"类别1",
           list:[
@@ -111,6 +113,13 @@ export default {
   methods:{
     back(){
       this.$router.go(-1)
+    },
+    test(){
+      const el = this.$refs.right
+      el.scrollTop += 600;
+    },
+    selectedHandle(index){
+      this.activeCategoryIndex = index
     }
   }
 }
@@ -121,14 +130,37 @@ export default {
   display:flex;
   height:700px;
 }
+
 .left-wrapper {
   flex:1;
   background-color: rgb(249, 190, 173);
   overflow-y: auto;
 }
+
+.left-wrapper-item {
+  height:60px;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.left-wrapper-item-active {
+  height:60px;
+  background-color: #fff;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .right-wrapper {
   flex:3;
-  background-color: rgb(177, 234, 253);
   overflow-y: auto;
+  /* padding-bottom:500px; */
+}
+
+.right-wrapper-item {
+  background-color: rgb(177, 234, 253);
+  margin:8px 16px 16px 16px;
+  height:600px;
 }
 </style>
