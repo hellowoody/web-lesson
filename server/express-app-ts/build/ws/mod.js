e34debd0dcd2fa3286fc35f9bf8f9202c9d519f3
woody <woodyhello@qq.com>
2020-10-21 10:19:12 +0800

    update

--- server/express-app-ts/build/ws/mod.js
+++ server/express-app-ts/build/ws/mod.js
@@ -4,8 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var ws_1 = __importDefault(require("ws"));
+var http_1 = __importDefault(require("http"));
+var url_1 = __importDefault(require("url"));
 var wss = new ws_1.default.Server({
-    port: 8080
+    noServer: true
+});
+var server = http_1.default.createServer(function (req, resp) {
+    var pathname = req.url;
+    console.log("http url: ", pathname);
+    if (pathname === "/send") {
+        // console.log(wss.clients)
+        wss.clients.forEach(function (client) {
+            client.send("服务器主动发送消息！");
+        });
+    }
+    resp.write("服务器主动发送消息！");
+    resp.end('服务器主动发送消息！');
 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
@@ -17,3 +31,13 @@ wss.on('connection', function connection(ws) {
     // console.log("client connection : ",ws)
     ws.send('后台已经知道连接成功了');
 });
+server.on("upgrade", function (req, socket, head) {
+    var pathname = url_1.default.parse(req.url).pathname;
+    console.log(pathname);
+    if (pathname === '/wss') {
+        wss.handleUpgrade(req, socket, head, function done(ws) {
+            wss.emit('connection', ws, req);
+        });
+    }
+});
+server.listen(8081, function () { return console.log("长连接服务启动了..."); });
--- server/express-app-ts/ws/mod.ts
+++ server/express-app-ts/ws/mod.ts
@@ -1,8 +1,23 @@
 import WebSocket from 'ws';
+import http from 'http';
+import url from 'url';
 
-const wss = new WebSocket.Server({ 
-    port: 8080 
+const wss = new WebSocket.Server({
+    noServer:true
 });
+
+const server = http.createServer((req,resp)=>{
+  const pathname = req.url
+  console.log("http url: ",pathname)
+  if(pathname === "/send"){
+    // console.log(wss.clients)
+    wss.clients.forEach(client=>{
+      client.send("服务器主动发送消息！")
+    })
+  }
+  resp.write("服务器主动发送消息！")
+  resp.end('服务器主动发送消息！')
+})
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
@@ -14,3 +29,15 @@ wss.on('connection', function connection(ws) {
 // console.log("client connection : ",ws)
   ws.send('后台已经知道连接成功了');
 });
+
+server.on("upgrade",function(req, socket, head){
+  const pathname = url.parse(req.url).pathname
+  console.log(pathname)
+  if (pathname === '/wss') {
+    wss.handleUpgrade(req, socket, head, function done(ws) {
+      wss.emit('connection', ws, req);
+    });
+  } 
+})
+
+server.listen(8081,()=>console.log("长连接服务启动了..."))
