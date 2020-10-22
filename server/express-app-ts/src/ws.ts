471d2ee45a17bf26d2e671efbfaf1df802dea966
woody <woodyhello@qq.com>
2020-10-22 09:37:47 +0800

    update

--- server/express-app-ts/src/ws.ts
+++ server/express-app-ts/src/ws.ts
@@ -9,6 +9,19 @@ const wsInstance = (server:http.Server)=>{
     wss.on('connection',ws => {
         ws.on('message',message => {
             console.log('接收到的消息: %s', message);
+            try {
+                const m_jsonObj= JSON.parse(message.toString())
+                if(m_jsonObj.hasOwnProperty("init")){
+                    //第一次连接 先不做处理
+                }else{
+                    // ws.send(`对 ${m_jsonObj.to} 说: ${m_jsonObj.msg}`)
+                    wss.clients.forEach(client=>{
+                        client.send(`对 ${m_jsonObj.to} 说: ${m_jsonObj.msg}`)
+                    })
+                }
+            } catch (e) {
+                console.log(e)
+            }
         })
         ws.on('close',()=>console.log("当前在线人数减少一位."))
     })
