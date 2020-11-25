82bf7fb1bd92dbe272efaa7bb782b37975a62bc2
woody <woodyhello@qq.com>
2020-11-25 14:36:21 +0800

    1

--- vue-cli/cli-demo/src/components/SearchResult.vue
+++ vue-cli/cli-demo/src/components/SearchResult.vue
@@ -5,13 +5,13 @@
       </template>
     </top-bar> 
   <h1>this is search result </h1>
-  <div v-for="item in dataList" :key="item.id">{{item.name}}</div>
+  <div v-for="(item,index) in dataList" :key="index">{{item.body}}</div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue'
-import { Http } from '@/kits/HttpMock.js'
-
+// import { Http } from '@/kits/HttpMock.js'
+import { Http } from '@/kits/Http.js'
 export default {
   name:"SearchResult",
   data(){
@@ -23,24 +23,58 @@ export default {
   components:{
     TopBar
   },
-  async created(){
+  created(){
     // console.log(this.$route)
     // this.searchContent = this.$route.query.searchInput
     this.searchContent = this.$route.params.searchInput
-
-    try {
-      const res = await Http("/dataList",{
-        count:3
-      })
-      this.dataList = res.data
-    } catch (err) {
-      console.log(err)
-    }
-
+    this.initData()
   },
   methods:{
     back(){
       this.$router.go(-1)
+    },
+    async initData(){
+      // try {
+      //   const res = await Http("/dataList",{
+      //     count:3
+      //   })
+      //   this.dataList = res.data
+      // } catch (err) {
+      //   console.log(err)
+      // }
+      try {
+        const pagenum = 5
+        const res = await Http("/graphql",{
+          query:`
+                query { 
+                    repository(name:"web-lesson",owner:"hellowoody"){
+                        name,
+                        description,
+                        issues (first:1,labels:["db"]) {
+                            nodes {
+                                id,
+                                title,
+                                body,
+                                bodyHTML,
+                                comments (last:${pagenum}) {
+                                    nodes {
+                                        body,
+                                    },
+                                    pageInfo {
+                                        hasPreviousPage
+                                    }
+                                }
+                            }
+                        },
+                    }
+                }
+            `
+        })
+        console.log(res)
+        this.dataList = res.data.data.repository.issues.nodes[0].comments.nodes
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
---
+++ vue-cli/cli-demo/src/kits/Http.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const instanceGithub = axios.create({
+    baseURL:"https://api.github.com",
+    timeout:10000,
+    headers:{
+        'Content-Type' : 'application/json',
+        'Authorization':'bearer '+"b128a0498e3aec05a44cac15a05f48e0276adebf"
+    }
+})
+
+instanceGithub.interceptors.request.use(function(config){
+    return config
+},function(e){
+    return Promise.reject(e)
+})
+
+instanceGithub.interceptors.response.use(resp => {
+    if(resp.status === 200){
+        return Promise.resolve(resp)
+    }
+},e => Promise.reject(e))
+
+export const Http = (api,param) => {
+    return new Promise((resolve,reject)=>{
+        instanceGithub.post(api,param)
+            .then(res => resolve(res))
+            .catch(e => reject(e))
+    })
+}
