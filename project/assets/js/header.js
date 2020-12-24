5d400bb75fa6253114c51a743a899e3e562d4278
woody <woodyhello@qq.com>
2020-12-24 14:22:27 +0800

    1

---
+++ project/assets/js/header.js
@@ -0,0 +1,15 @@
+$(function(){
+    ;(function(){
+        let shop = $("#h_m_r_shop")
+        let shop_hide = $("#h_m_r_s_hide")
+        shop.hover(function(){
+            shop_hide.stop(true).slideDown() //链式
+        },function(){
+            shop_hide.stop(true).slideUp()
+        })
+     })() // js 闭包 立即执行函数
+}); // jquery for ready
+
+
+
+
