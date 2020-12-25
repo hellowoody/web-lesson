057dcc8d260d9e000941fca4460dc418103d0fed
woody <woodyhello@qq.com>
2020-12-25 10:44:35 +0800

    1

--- project/assets/js/banner.js
+++ project/assets/js/banner.js
@@ -4,6 +4,7 @@ $(function(){
         var picli = $("#b_main .b_m_pic li");
         var len = picli.length ; 
         var btn = $("#b_main .b_m_btn div")
+        var tabli = $("#b_main .b_m_tab ul li");
 
         btn.click(function(){
             var index = $(this).index()
@@ -13,18 +14,24 @@ $(function(){
                     _index = 0
                 }
                 //图片动起来
-                imgPLay()
+                imgPlay()
             }else{
                 _index-- // _index = _index - 1
                 if(_index < 0){
                     _index = len - 1
                 }
                 //图片动起来
-                imgPLay()
+                imgPlay()
             }
         })
 
-        function imgPLay(){
+        tabli.click(function(){
+            _index = $(this).index()
+            imgPlay()
+        })
+
+        function imgPlay(){
+            tabli.eq(_index).addClass("clicked").siblings().removeClass("clicked")
             picli.eq(_index).fadeIn().siblings().fadeOut();
         }
 
