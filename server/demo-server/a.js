de9117dacd4d92b895d77df3ae852e1b180fc017
woody <woodyhello@qq.com>
2021-01-03 11:38:38 +0800

    1

--- server/demo-server/a.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const {defaultapi} = require("./controller")
-const app = express()
-
-app.get("/api",defaultapi)
-
-app.listen(9000,()=>{
-    console.log("9000 PORT successfully started up!")
-})
--- server/demo-server/controller.js
+++
@@ -1,8 +0,0 @@
-const defaultapi = function(req,resp){
-    console.log(req.query)
-    resp.send("hello node api")
-}
-
-module.exports = {
-    defaultapi
-}
