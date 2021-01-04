1bde7db1ea0179b286a8886ddd9c710bd744a5b8
woody <woodyhello@qq.com>
2021-01-04 16:35:38 +0800

    1

--- project/demo/pages/home/home.js
+++ project/demo/pages/home/home.js
@@ -1,7 +1,5 @@
-// pages/home/home.js
-
-const app = getApp()
 Page({
+  
 
   /**
    * 页面的初始数据
@@ -14,55 +12,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(1000)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log(2000)
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log(3000)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+      console.log(4000)
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log(5000)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
