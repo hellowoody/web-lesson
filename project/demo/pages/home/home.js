f653e504bb5451c040203109ade10a65c7cfd1d6
woody <woodyhello@qq.com>
2021-01-04 16:58:26 +0800

    1

--- project/demo/pages/home/home.js
+++ project/demo/pages/home/home.js
@@ -8,38 +8,8 @@ Page({
     msg:""
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(1000)
-  },
+  test(){
+    console.log(this.data.msg)
+  }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    console.log(2000)
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    console.log(3000)
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-      console.log(4000)
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    console.log(5000)
-  },
 })
