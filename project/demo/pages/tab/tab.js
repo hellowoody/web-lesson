189db019f4da2afc64c9f9e187b4020a4c199b47
woody <woodyhello@qq.com>
2021-01-05 16:45:56 +0800

    1

--- project/demo/pages/tab/tab.js
+++ project/demo/pages/tab/tab.js
@@ -2,6 +2,32 @@
 Page({
 
   data: {
+    tabs:[
+      {
+        id:1,
+        name:"选项卡一",
+        isActive:true,
+        content:"内容一"
+      },
+      {
+        id:2,
+        name:"选项卡二",
+        isActive:false,
+        content:"内容二"
+      },
+      {
+        id:3,
+        name:"选项卡三",
+        isActive:false,
+        content:"内容三"
+      },
+      {
+        id:4,
+        name:"选项卡四",
+        isActive:false,
+        content:"内容四"
+      },
+    ],
     activeIndex : 1
   },
 
