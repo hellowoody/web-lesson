b597de5ed6e9e4ec1486595c7c098b8d433c91b0
woody <woodyhello@qq.com>
2021-01-05 10:40:29 +0800

    1

--- project/demo/pages/excrise/excrise.js
+++ project/demo/pages/excrise/excrise.js
@@ -1,66 +1,25 @@
-// pages/excrise/excrise.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    forFlag:true,
+    ifFlag:false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showComp(e){
+    console.log(e)
+    switch (e.target.dataset.name) {
+      case "for":
+        this.setData({
+          forFlag:true,
+          ifFlag:false
+        })
+        break;
+      case "if":
+        this.setData({
+          forFlag:false,
+          ifFlag:true
+        })
+        break;
+    }
   }
 })
