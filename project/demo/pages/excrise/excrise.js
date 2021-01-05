80358a51160142a9f82c63b9a87b0549153abcdb
woody <woodyhello@qq.com>
2021-01-05 10:54:57 +0800

    1

--- project/demo/pages/excrise/excrise.js
+++ project/demo/pages/excrise/excrise.js
@@ -1,23 +1,20 @@
 Page({
 
   data: {
-    forFlag:true,
-    ifFlag:false
+    showTarget:"for",
   },
 
   showComp(e){
-    console.log(e)
+    // console.log(e)
     switch (e.target.dataset.name) {
       case "for":
         this.setData({
-          forFlag:true,
-          ifFlag:false
+            showTarget : e.target.dataset.name
         })
         break;
       case "if":
         this.setData({
-          forFlag:false,
-          ifFlag:true
+          showTarget : e.target.dataset.name
         })
         break;
     }
