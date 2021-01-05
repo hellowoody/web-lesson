50864d1b275fc54a7c67130a93cf088e1392d708
woody <woodyhello@qq.com>
2021-01-05 10:18:19 +0800

    1

--- project/demo/components/ForTest/ForTest.js
+++ project/demo/components/ForTest/ForTest.js
@@ -4,14 +4,18 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    arr:[
+      {id:1,name:"张三"},
+      {id:2,name:"李四"},
+      {id:3,name:"王武"},
+    ]
   },
 
   /**
