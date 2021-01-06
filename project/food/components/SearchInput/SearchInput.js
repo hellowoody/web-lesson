1440cd26f5a3ab5ff09a985ffb3743afad771121
woody <woodyhello@qq.com>
2021-01-06 15:00:06 +0800

    1

--- project/food/components/SearchInput/SearchInput.js
+++ project/food/components/SearchInput/SearchInput.js
@@ -1,23 +1,8 @@
-// components/SearchInput/SearchInput.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
 
-  },
+  properties: {},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+  data: {},
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+  methods: {}
 })
--- project/food/pages/home/home.js
+++ project/food/pages/home/home.js
@@ -1,66 +1,39 @@
-// pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    categroys:[
+      {
+        id:1,
+        name:"全部",
+        imgPath:"/assets/imgs/t/Chinese_image.png"
+      },
+      {
+        id:2,
+        name:"中餐",
+        imgPath:"/assets/imgs/t/Chinese_image.png"
+      },
+      {
+        id:3,
+        name:"法餐",
+        imgPath:"/assets/imgs/t/French_image.png"
+      },
+      {
+        id:4,
+        name:"印餐",
+        imgPath:"/assets/imgs/t/Indian_image.png"
+      },
+      {
+        id:5,
+        name:"快餐",
+        imgPath:"/assets/imgs/t/american_image.png"
+      },
+      {
+        id:6,
+        name:"其他",
+        imgPath:"/assets/imgs/t/american_image.png"
+      },
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
