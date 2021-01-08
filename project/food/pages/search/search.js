5ae02cda4bd3caba833ceb1e0b04672d65a46da4
woody <woodyhello@qq.com>
2021-01-08 17:16:32 +0800

    1

--- project/food/pages/search/search.js
+++ project/food/pages/search/search.js
@@ -2,12 +2,38 @@ Page({
   searchInput:"",
 
   data: {
+    searchHistory:[]
+  },
+
+  onLoad(){
+    this.setData({
+      searchHistory:wx.getStorageSync('searchHistory') ? wx.getStorageSync('searchHistory') : []
+    })
   },
 
   goSearch(){
-    wx.navigateTo({
-      url: '/pages/searchResult/searchResult?p='+this.searchInput
-    })
+    if (this.searchInput === "") {
+      wx.showToast({
+        title:"请输入查询内容",
+        icon:'none'
+      })
+    }else{
+      const searchHistory = wx.getStorageSync("searchHistory");
+      if (searchHistory){
+        if (searchHistory.indexOf(this.searchInput) < 0) {
+          searchHistory.push(this.searchInput)
+          wx.setStorageSync('searchHistory', searchHistory)
+        }
+      }else{
+        wx.setStorageSync('searchHistory', [this.searchInput])
+      }
+      this.setData({
+        searchHistory:wx.getStorageSync('searchHistory') ? wx.getStorageSync('searchHistory') : []
+      })
+      wx.navigateTo({
+        url: '/pages/searchResult/searchResult?p='+this.searchInput
+      })
+    } 
   },
   
   searchHandle(e){
