ddcdd32ca819fcb1fd7e5a7e19363eed14a04241
woody <woodyhello@qq.com>
2021-03-09 10:13:37 +0800

    1

---
+++ exercise/t.js
@@ -0,0 +1,30 @@
+
+let b = navigator.getBattery().then((b) => console.log(b));
+
+
+//(b) => console.log(b)
+// obj JS type function 
+// obj()
+
+function getAge(id,name){
+    console.log(arguments.id)
+    console.log(arguments.name)
+    
+}
+
+window.navigator = {
+    appName:"123",
+    platform:"456",
+    getBattery:function(){
+        console.log(789)
+
+        return new Promise()
+    }
+}
+
+/*
+
+es6  promise 处理/形容 异步操作
+
+arrow function
+*/
