c56a0d286dbedfef26c1c8077b010ffd3dae8348
woody <woodyhello@qq.com>
2021-09-09 15:03:25 +0800

    day10-3

--- project/vue3-exercise/vite.config.js
+++ project/vue3-exercise/vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from "path"
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"./",
-  plugins: [
-    vue(),
-    Components({ 
-      resolvers: [
-        AntDesignVueResolver(),
-      ]
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias:{
       '@':resolve(__dirname,"src")  // __dirname 当前配置文件所在的系统绝对路径
